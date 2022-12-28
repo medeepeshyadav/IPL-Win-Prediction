48f3466492fe31d89ec92599d49af71a387c56b4
Deepesh <deepeshonearth@gmail.com>
2022-12-28 13:16:18 +0530

    deleted old files

--- API/.ipynb_checkpoints/test-checkpoint.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-BASE = "http://127.0.0.1:5000/"
-
-response1 = requests.get(BASE +"helloworld/deepesh/24") 
-print(response1.json())
--- API/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-from flask import json
-
-BASE = "mynameislakhan"
-
-response1 = requests.post(BASE +"") 
-print(response1.json())
